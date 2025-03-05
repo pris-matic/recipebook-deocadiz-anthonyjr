42dfd8ec5e238b65e43dd563dd49e2abaa6d960c
pris-matic <deocadizjranthony@gmail.com>
2025-03-05 15:47:10 +0800

    registered models, added filtering, inline editing, and search fields

--- ledger/admin.py
+++ ledger/admin.py
@@ -1,3 +1,34 @@
 from django.contrib import admin
+from .models import Ingredient, Recipe, RecipeIngredient
 
 # Register your models here.
+class RecipeIngredientInline(admin.TabularInline):
+    model = RecipeIngredient
+    extra = 0
+
+class IngredientAdmin(admin.ModelAdmin):
+    model = Ingredient
+
+    list_display = ('name',)
+    list_filter = ('name',)
+    search_fields =('name',)
+
+class RecipeAdmin(admin.ModelAdmin):
+    model = Recipe
+
+    list_display = ('name',)
+    list_filter = ('name',)
+    search_fields =('name',)
+    inlines = [RecipeIngredientInline]
+
+class RecipeIngredientAdmin(admin.ModelAdmin):
+    model = RecipeIngredient
+
+    list_display = ('recipe', 'ingredient', 'quantity')
+    list_filter = ('recipe', 'ingredient',)
+    search_fields =('recipe__name', 'ingredient__name',)
+
+
+admin.site.register(Ingredient,IngredientAdmin)
+admin.site.register(Recipe,RecipeAdmin)
+admin.site.register(RecipeIngredient,RecipeIngredientAdmin)
