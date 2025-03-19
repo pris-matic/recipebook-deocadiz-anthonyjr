96a1ddad74e9c38cd7f40c3b5b9e141bbd5034a6
pris-matic <deocadizjranthony@gmail.com>
2025-03-20 00:41:42 +0800

    added Profile model and updated Recipe model

--- ledger/models.py
+++ ledger/models.py
@@ -1,5 +1,6 @@
 from django.db import models
 from django.urls import reverse
+from django.contrib.auth.models import User
 
 # Create your models here.
 class Ingredient(models.Model):
@@ -7,15 +8,29 @@ class Ingredient(models.Model):
 
     def __str__(self):
         return self.name
-    
+
+class Profile(models.Model):
+    user = models.OneToOneField(User,on_delete=models.CASCADE)
+    name = models.CharField(max_length=50)
+    bio = models.CharField(max_length=255)
+
+    def __str__(self):
+        return self.user.username
+
 class Recipe(models.Model):
     name = models.CharField(max_length=50)
+    author = models.ForeignKey(Profile, on_delete=models.CASCADE)
+    createdOn = models.DateTimeField(auto_now_add=True)
+    updatedOn = models.DateTimeField(auto_now=True)
 
     def __str__(self):
         return self.name
     
     def get_absolute_url(self):
         return reverse('ledger:getRecipe',args=[self.pk])
+    
+    class Meta:
+        ordering = ['name']
 
 class RecipeIngredient(models.Model):
     recipe = models.ForeignKey(Recipe, on_delete=models.CASCADE)
