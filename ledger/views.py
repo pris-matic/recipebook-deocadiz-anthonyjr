cb9bf349d0d8cb6594c6c4decefd09d6bb4d0223
pris-matic <deocadizjranthony@gmail.com>
2025-03-20 00:40:39 +0800

    some views now require user log on

--- ledger/views.py
+++ ledger/views.py
@@ -1,13 +1,19 @@
 from django.shortcuts import render, HttpResponse
 from django.template import loader
 from .models import Recipe, RecipeIngredient
+from django.contrib.auth.decorators import login_required
 
 # Create your views here.
 
+def homepage(request):
+    return render(request,'homepage.html')
+
+@login_required
 def recipesInDatabase(request):
     items = Recipe.objects.all()
     return render(request, 'recipebook.html', {'recipes': items})
 
+@login_required
 def getRecipe(request,num):
     recipe = Recipe.objects.get(id=num)
     ingredients = RecipeIngredient.objects.filter(recipe=recipe)
