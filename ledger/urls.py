e66ea4841328d9714271bfa0707237ad0c80ffb9
pris-matic <deocadizjranthony@gmail.com>
2025-03-20 00:40:58 +0800

    added url for homepage

--- ledger/urls.py
+++ ledger/urls.py
@@ -1,7 +1,8 @@
 from django.urls import path
-from .views import recipesInDatabase, getRecipe
+from .views import recipesInDatabase, getRecipe, homepage
 
 urlpatterns = [
+    path('home/', homepage, name='home'),
     path('recipes/list/', recipesInDatabase, name = 'recipebook'),
     path('recipe/<int:num>/', getRecipe, name='getRecipe'),
 ]
