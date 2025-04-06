c59ddd80c77f50eabdae4eda04ac593fd121e263
pris-matic <deocadizjranthony@gmail.com>
2025-04-07 00:10:29 +0800

    added url for adding items

--- ledger/urls.py
+++ ledger/urls.py
@@ -1,10 +1,13 @@
 from django.urls import path
-from .views import recipesInDatabase, getRecipe, homepage
+from .views import recipesInDatabase, getRecipe, homepage, addRecipe,addIngredient,addImage
 
 urlpatterns = [
     path('home/', homepage, name='home'),
     path('recipes/list/', recipesInDatabase, name = 'recipebook'),
     path('recipe/<int:num>/', getRecipe, name='getRecipe'),
+    path('recipe/add/', addRecipe, name='addRecipe'),
+    path('recipe/add/ingredient', addIngredient, name='addIngredient'),
+    path('recipe/<int:num>/add_image', addImage, name='addImage'),
 ]
 
 app_name = 'ledger'
